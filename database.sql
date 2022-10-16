04035839a6bd6993a760c418a13784e7d9d6aeae
rohan19a <arohan19a2@gmail.com>
2022-10-15 23:20:07 -0700

    a

--- database.sql
+++ database.sql
@@ -1,11 +1,8 @@
 CREATE TABLE tasks (
-    id INT PRIMARY KEY,
-    title VARCHAR(500),
-    dates DATETIME,
-    dueDate DATETIME,
-    timeNeeded INT,
-    vals FLOAT(10, 10),
+    id INT PRIMARY KEY, -- 1-x 
+    title VARCHAR(500), --task they need to do
+    dates DATETIME, -- time they entered it
+    dueDate DATETIME, -- due date
+    timeNeeded INT, --estimated time needed
+    vals FLOAT(10, 10), --alorithm value
 )
-
-
-
