e914d4339d55e0167665d47257c3fede4cbab5d8
rohan19a <arohan19a2@gmail.com>
2022-10-15 18:03:00 -0700

    test

---
+++ script.js
@@ -0,0 +1,39 @@
+function Add() {
+    console.log("h");
+}
+
+//D : Due date 
+//f : Expected Amount of time to finish 
+//F : Current time
+
+/*
+(-log(dead line - current time)
+
+P (d, f, t)
+
+D - t = delta(t)
+
+p(f, delta(t)) */
+
+
+
+
+
+
+function item (name, progress, timeToEnd, deadline) {
+    var time = new Date();
+    this.name = name;
+    this.progress = progress;
+    this.timeToEnd = timeToEnd;
+    this.deadline = deadline;
+    
+
+
+
+}
+
+function priority(f, delta) {
+    let logVal = -1 * Math.log(delta);
+
+
+}
